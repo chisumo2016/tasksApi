a9ae5565fe2238d069ddbd7a9be01d524027c83c
Bernard Chisumo <bchisumo74@gmail.com>
2025-12-09 14:44:06 +0000

    feat: Add Rate Limiting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,9 @@ namespace App\Providers;
 use Dedoc\Scramble\Scramble;
 use Dedoc\Scramble\Support\Generator\OpenApi;
 use Dedoc\Scramble\Support\Generator\SecurityScheme;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,9 +26,17 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Scramble::afterOpenApiGenerated(function (OpenApi $openApi) {
-                $openApi->secure(
-                        SecurityScheme::http('bearer', 'BearerAuth')
+            $openApi->secure(
+                SecurityScheme::http('bearer', 'BearerAuth')
             );
         });
+
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(200)
+                ->by($request->user() ? $request->user()->id : $request->ip());
+            //return Limit::perMinute(2)->by($request->user()?->id : $request->ip());
+            //what is the authenticate user is admin?
+        });
+
     }
 }
